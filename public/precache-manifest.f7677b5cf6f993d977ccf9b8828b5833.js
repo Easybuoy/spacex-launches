3bc85e768ed1ccfe2d582acc2e8f4ca9c834dfd7
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-05-18 12:39:25 +0100

    create launch component

---
+++ public/precache-manifest.f7677b5cf6f993d977ccf9b8828b5833.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5768fff5dadc675e581cb40fbc6613d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "169a16ce4307d42a99cf",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "07aa567cabdac326f2c8",
+    "url": "/static/js/2.ba9452d6.chunk.js"
+  },
+  {
+    "revision": "169a16ce4307d42a99cf",
+    "url": "/static/js/main.44b4a360.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
