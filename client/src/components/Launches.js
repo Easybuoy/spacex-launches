77943fc5da573c3c8eedc03edac8bf83e5c3a0c7
Ezekiel <ekunolaeasybuoy@gmail.com>
2019-05-18 10:18:05 +0100

    setup react

---
+++ client/src/components/Launches.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+
+const LAUNCHES_QUERY = gql`
+{
+    
+}
+`;
+
+
+export class Launches extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default Launches
